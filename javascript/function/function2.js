78a3ce2f566d6b53a68b82483e18499ac11e0ea2
김어진 <asas0066@naver.com>
2025-02-11 18:57:23 +0900

    1111111

--- javascript/function/function2.js
+++ javascript/function/function2.js
@@ -5,6 +5,28 @@
  *  기능: 배열의 요소중에서 제일 큰값을 콘솔출력.
  */
 
+// let ary1 = [3, 15, 22, 7];
+// let ary2 = [23, 11, 45, 30];
+// let ary3 = [];
+// for (let i = 0; i < 5; i++) {
+//   ary3[i] = parseInt(Math.random() * 50);
+// }
+
+// function getMax(e = []) {
+//   let sum = 0;
+//   for (let j = 0; j < e.length; j++) {
+//     if (sum < e[j]) {
+//       sum = e[j]
+//     }
+//   }
+//   console.log(sum);
+//   return sum; // 함수를 호출한 영역으로 반환. (26번처럼 반환시켜서 화면에 보여줌)
+// } // end of getMax()
+
+// document.write(getMax(ary1)); // => return이 없으면 화면에 보여지지 않음        
+// alert(getMax(ary2)); // 위의 함수식에 ary2을 넣어서 조건을 실행시킴.
+// getMax(ary3); // 위의 함수식에 ary3을 넣어서 조건을 실행시킴.
+console.log("--------------------------")
 let ary1 = [3, 15, 22, 7];
 let ary2 = [23, 11, 45, 30];
 let ary3 = [];
@@ -12,17 +34,15 @@ for (let i = 0; i < 5; i++) {
   ary3[i] = parseInt(Math.random() * 50);
 }
 
-function getMax(e = []) {
-  let sum = 0;
-  for (let j = 0; j < e.length; j++) {
-    if (sum < e[j]) {
-      sum = e[j]
+function getMax(a = []) {
+  let b = 0;
+  for (let j = 0; j < a.length; j++)
+    if (b < a[j]) {
+      b = a[j];
     }
-  }
-  // console.log(sum);
-  return sum; // 함수를 호출한 영역으로 반환. (26번처럼 반환시켜서 화면에 보여줌)
-} // end of getMax()
-
-document.write(getMax(ary1)); // => return이 없으면 화면에 보여지지 않음        
-alert(getMax(ary2)); // 위의 함수식에 ary2을 넣어서 조건을 실행시킴.
-getMax(ary3); // 위의 함수식에 ary3을 넣어서 조건을 실행시킴.
+    console.log(b);
+    return b;
+}
+getMax(ary1);
+getMax(ary2);
+getMax(ary3);
--- javascript/function/function4.js
+++ javascript/function/function4.js
@@ -1,19 +1,25 @@
 // function4.js
 
 // 이벤트 - 이벤트핸들러.
+// let item = document.querySelector('button');
+// console.log(item);
+
+// item.addEventListener('click', function () {
+//   alert('마우스가 클릭됨');
+// });
+
+// item.addEventListener('mouseover', function () {
+//   item.style.backgroundColor = 'yellow';
+//   // item.style.color = 'red';
+// });
+
+// item.addEventListener('mouseout', function(){
+//   item.style.backgroundColor = '';
+//   // item.style.Color = '';
+// });
+
 let item = document.querySelector('button');
-console.log(item);
-
-item.addEventListener('click', function () {
-  alert('마우스가 클릭됨');
-});
-
-item.addEventListener('mouseover', function () {
-  item.style.backgroundColor = 'yellow';
-  // item.style.color = 'red';
-});
-
-item.addEventListener('mouseout', function(){
-  item.style.backgroundColor = '';
-  // item.style.Color = '';
-});
+item.addEventListener('click', function() {
+  let IP = 0;
+  f
+})
